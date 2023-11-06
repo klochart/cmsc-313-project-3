0a82600b8485e9ab1819e4fd632e8c2d1519f943
klochart <thestudyingdragon365@gmail.com>
2023-11-05 22:27:56 -0500

    add reverse.asm

---
+++ reverse.asm
@@ -0,0 +1,61 @@
+section .data
+
+wordPrompt:     db "Please enter the text: ", 0
+wordPromptLen:  equ $- wordPrompt
+
+numPrompt:      db "Enter a number between 2 and the total number of characters in the string ", 0
+numPromptLen:   equ $- numPrompt
+
+newLine:        db 10
+
+section .bss
+
+string:         resb 64
+num:            resb 2
+
+section .text
+
+global main
+
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, wordPrompt 
+    mov rdx, wordPromptLen
+    syscall
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, string
+    mov rdx, 256
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, numPrompt 
+    mov rdx, numPromptLen
+    syscall
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, num
+    mov rdx, 256
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, string
+    mov rdx, 64
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, num
+    mov rdx, 2
+    syscall
+
+
+
+exit:
+    mov rax, 60
+    xor rdi, rdi
